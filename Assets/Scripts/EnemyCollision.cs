085b3cb2b420e66b60bf4688fd2bee9a26a9168f
sworley8 <sophiaworley@hotmail.com>
2019-08-21 23:28:48 -0400

    first command

---
+++ Assets/Scripts/EnemyCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Do something else here11");
+        if (collision.gameObject.tag == "Player")
+        {
+            //If the GameObject has the same tag as specified, output this message in the console
+            Debug.Log("Do something else here");
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/KamaMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KamaMovement : MonoBehaviour
+{
+    public Quaternion originalRotationVal;
+    public float velocity = 4;
+    //move in local rotation with respect to world
+    void Movement()
+    {
+        transform.Translate(transform.forward * Time.deltaTime * velocity, Space.World);
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalRotationVal = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    void OnDrawGizmos()
+    {
+        Vector3 forward = transform.forward * 10;
+        Debug.DrawRay(transform.position, forward, Color.yellow);
+    }
+}
