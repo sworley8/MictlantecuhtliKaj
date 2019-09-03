daea3f120f887d99b9535bc5d298840bf3da8989
sworley8 <sophiaworley@hotmail.com>
2019-09-02 23:36:50 -0400

    Add files via upload

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Image HealthCircle;
+    public int maxhealth = 100;
+    private int curr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealthCircle = GameObject.FindWithTag("Health").GetComponent<Image>();
+        curr = maxhealth;
+    }
+
+    private void handleHealth(int damage)
+    {
+        curr -= damage;
+        HealthCircle.fillAmount = (float)curr / (float)maxhealth;
+    }
+    //void TakeDamage(float damage)
+    //{
+    //    curr = curr - damage;
+    //    HealthCircle.fillAmount = curr;
+
+    //}
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag != "Player")
+        {
+            if (curr > 0)
+            {
+                handleHealth(10);
+                Debug.Log("Do something else here");
+
+            }
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            handleHealth(10);
+        }
+    }
+
+}
