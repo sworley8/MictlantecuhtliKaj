e032df3c09b2d43d149999735fd6e93b2bb21351
sworley8 <sophiaworley@hotmail.com>
2019-09-13 00:39:41 -0400

    Adding my spline

---
+++ Assets/Scripts/PutTogetherStuff.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutTogetherStuff : MonoBehaviour
+{
+	//// Start is called before the first frame update
+	void Start()
+	{
+		Jun_BezierCurve curve = gameObject.GetComponentInChildren(typeof(Jun_BezierCurve)) as Jun_BezierCurve;
+		if (curve != null)
+		{
+			while (m_bezierPoints != null)
+			{
+				m_bezierPoints.transform.localPosition = m_bezierPoints.transform.localPosition - gameObject.transform.localPosition;
+
+			}
+		}
+	}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    //subtract empty object from each point in the array
+    //while (m_bezierPoints != null) {
+    //    m_bezierPoints.transform.localPosition = m_bezierPoints.transform.localPosition - gameObject.transform.localPosition
+    //}
+}
