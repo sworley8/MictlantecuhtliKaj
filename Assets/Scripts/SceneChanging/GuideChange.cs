cbe158450b765cc714ccf082529cb225d465cfef
sworley8 <sophiaworley@hotmail.com>
2019-11-18 05:11:52 -0500

    guide page

---
+++ Assets/Scripts/SceneChanging/GuideChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GuideChange : MonoBehaviour
+{
+    //public float timerBack = 20f;
+    public void menuKaj()
+    {
+        SceneManager.LoadScene(3);
+
+    }
+    public void guideKaj()
+    {
+        SceneManager.LoadScene(9999);
+
+    }
+}
