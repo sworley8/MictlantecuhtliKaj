574565d45c46a8e04f6c33e3336b62e7a3c7f9de
sworley8 <sophiaworley@hotmail.com>
2019-11-20 15:57:12 -0500

    sound and jellyfish fixed up

--- Assets/Scripts/ChangeSound.cs
+++ Assets/Scripts/ChangeSound.cs
@@ -2,26 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.UIElements;
+//using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class ChangeSound : MonoBehaviour
 {
 
     public AudioMixer mixer;
-    public Slider slider;
-    //void Start()
-    //{
-    //    slider.value = PlayerPrefs.GetFloat("MusicVolume", 0.95f);
-    //}
+    public Slider sliderMusic;
+    public Slider sliderSFX;
+    void Start()
+    {
+        sliderMusic.value = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        sliderSFX.value = PlayerPrefs.GetFloat("SFXVolume", 1f);
+
+    }
     public void SetMusicLevel()
     {
-        mixer.SetFloat("MusicVol", Mathf.Log10(slider.value) * 20);
-        //Debug.Log(slider.value);
-        //PlayerPrefs.SetFloat("MusicVolume", values);
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderMusic.value) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderMusic.value);
     }
     public void SetSFXLevel()
     {
-        mixer.SetFloat("SFXVol", Mathf.Log10(slider.value) * 20);
+        mixer.SetFloat("SFXVol", Mathf.Log10(sliderSFX.value) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", sliderSFX.value);
     }
 
 }
