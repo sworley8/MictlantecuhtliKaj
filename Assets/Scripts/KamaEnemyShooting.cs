7b82eb094e6aa5635af66ac4d2752f8016871861
sworley8 <sophiaworley@hotmail.com>
2019-11-22 04:26:31 -0500

    added enemy flashing

--- Assets/Scripts/KamaEnemyShooting.cs
+++ Assets/Scripts/KamaEnemyShooting.cs
@@ -25,14 +25,18 @@ public class KamaEnemyShooting : MonoBehaviour
     private Vector3 prevPlayerPos;
     public float rotationSpeed;
     public Vector3 lazerOffset;
-    //public AudioSource shoot;
-    //public GameObject puff;
+    public AudioSource shoot;
+    public GameObject puff;
+    Material[] mats;
+    Color originalColor;
     // Start is called before the first frame update
     void Start()
     {
         //shotLine = GetComponentInChildren<LineRenderer>;
         //kamaEnemy = GameObject.FindGameObjectWithTag("Enemy").transform;
         scaledDamage = maxDamage - minDamage;
+        mats = puff.GetComponent<MeshRenderer>().materials;
+        originalColor = mats[0].color;
 
     }
 
@@ -52,54 +56,48 @@ public class KamaEnemyShooting : MonoBehaviour
             transform.rotation = Quaternion.LookRotation( delta, Vector3.up);
             //Debug.Log(player.position);
             targetStart -= Time.deltaTime;
+            //mats[0].color = Color.red;
+            if (targetStart >= 0.5f)
+            {
+                mats[0].color = originalColor;
+            } else
+            {
+                mats[0].color = Color.magenta;
+            }
             if (targetStart <= targetEnd)
             {
+                
                 reloadTime -= Time.deltaTime;
                 if (reloadTime <= 0f)
                 {
-
+        
                     Shootings();
-                    reloadTime = 0.2f;
+                    reloadTime = 0.25f;
                     targetStart = 2f;
                 }
+
+                    
+                //if (reloadTime <= 0f)
+                //{
+                //    Shootings();
+                //    reloadTime = 0.2f;
+                //    targetStart = 2f;
+                //}
+     
+
             }
             prevPlayerPos = player.transform.position;
         }
-        //if (!shots && (reloadTime != 0))
-        //{
-        //    Shootings();
-        //}
-        //if (reloadTime == 0)
-        //{
-        //    reloadTime = 3f;
-        //}
     }
     void Shootings()
     {
         GameObject laser3 = GameObject.Instantiate(m_shotPrefab, gunNozzle3.position, gunNozzle3.rotation) as GameObject;
         GameObject laser4 = GameObject.Instantiate(m_shotPrefab, gunNozzle4.position, gunNozzle4.rotation) as GameObject;
-        //shoot.Play();
+        shoot.Play();
         GameObject.Destroy(laser3, 3f);
         GameObject.Destroy(laser4, 3f);
         //shots = true;
         int layerMask = 1 << 13;
-
-        // This would cast rays only against colliders in layer 8.
-        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
-
-        //RaycastHit hit;
-        //// Does the ray intersect any objects excluding the player layer
-        //if (Physics.BoxCast(transform.position, new Vector3(.5f, .5f, .5f), transform.forward, out hit, transform.rotation, Mathf.Infinity, layerMask))
-        //{
-        //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-        //    float distanceBetween = Vector3.Distance(transform.position, laser3.transform.position) / 200;
-        //    Debug.Log("I am : " + gameObject.name +  " and I hit: " + hit.transform.gameObject.name);
-        //    int damage = (int)(scaledDamage * distanceBetween + minDamage);
-        //    if (hit.transform.tag == "Player")
-        //    {
-        //        ph.handleHealth(damage);
-        //    }
-        //}
     }
     void OnDrawGizmos()
     {
